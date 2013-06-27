491c34c0de6d9b54f4e93933dce021ba5ba416e1
David Ewald <ewadav@uw.edu>
2013-06-26 22:07:38 -0700

    Initial Commit

---
+++ Breakout/src/breakout/Brick.java
@@ -0,0 +1,31 @@
+package breakout;
+
+import javax.swing.ImageIcon;
+
+public class Brick extends Sprite{
+
+	String brickie = "Breakout/Images/brick.png";
+	boolean destroyed;
+	
+	public Brick(int x, int y)	{
+		this.x = x;
+		this.y = y;
+		
+		ImageIcon ii = new ImageIcon(this.getClass().getResource(brickie));
+		image = ii.getImage();
+		
+		width = image.getWidth(null);
+		height = image.getHeight(null);
+		
+		destroyed = false;
+	}
+	
+	public boolean isDestroyed() {
+      return destroyed;
+    }
+
+    public void setDestroyed(boolean destroyed)	{
+      this.destroyed = destroyed;
+    }
+    
+}
